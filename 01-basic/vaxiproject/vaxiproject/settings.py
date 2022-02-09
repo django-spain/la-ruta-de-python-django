a4bb9edad4a7a72208f959a4c062c4afe0d9a3b0
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-02-09 23:14:31 +0100

    sc

--- 01-basic/vaxiproject/vaxiproject/settings.py
+++ 01-basic/vaxiproject/vaxiproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios.apps.UsuariosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,6 +123,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'vaxiproject/static',
 ]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
